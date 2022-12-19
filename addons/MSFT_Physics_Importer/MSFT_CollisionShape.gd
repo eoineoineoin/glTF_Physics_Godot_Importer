9f4779c6c540d8f20095b33fd737140706ee4df4
Eoin Mcloughlin <hkeoin@eoinrul.es>
2022-12-19 12:09:29 +0000

    Add missed file

---
+++ addons/MSFT_Physics_Importer/MSFT_CollisionShape.gd
@@ -0,0 +1,26 @@
+extends Node3D
+class_name MSFT_CollisionShape
+
+@export var shape : Shape3D
+
+# This is a container very similar to CollisionShape3D, which 
+# does not need to be the immediate child of a CollisionObject3D
+# (i.e., will work for a grandparent CollisionObject3D)
+func _ready():
+	if shape == null:
+		return
+
+	var bodyFromShape : Transform3D = transform
+
+	var parent : Node3D = get_parent()
+	while parent != null:
+		if parent as CollisionObject3D:
+			break
+		bodyFromShape = parent.transform * bodyFromShape
+		parent = parent.get_parent()
+
+	if parent != null:
+		var body : CollisionObject3D = parent as CollisionObject3D
+		var ownerId = body.create_shape_owner(self)
+		body.shape_owner_add_shape(ownerId, shape)
+		body.shape_owner_set_transform(ownerId, bodyFromShape)
